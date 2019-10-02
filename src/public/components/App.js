9d90c1783ad6566ffb0d43b90656f043e7d9c64d
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-02 15:39:56 +0300

    First commit.

---
+++ src/public/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Repositories from './Repositories';
+
+const App = ({ repositories }) => (
+  <Repositories repositories={repositories} />
+);
+
+export default App;
